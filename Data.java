2cb4ac05ff4e76216da86d49b5b3f78556a5316f
aquablue31 <90348603+aquablue31@users.noreply.github.com>
2022-03-06 21:37:22 -0500

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,31 @@
+public class Data 
+{
+   /**
+      Computes the average of the measures of the given objects.
+      @param objects an array of Measurable objects
+      @return the average of the measures
+   */
+  public static double average(Measurable[] objects)
+  {
+     double sum = 0;
+     double limit = 1000;
+     int count = 0; 
+     for (Measurable obj : objects)
+     {
+        if (obj instanceof BankAccount) {
+           Filter X = (Filter) obj; 
+           if (X.accept(limit)) {
+              sum = sum + obj.getMeasure();
+              count++;
+           }
+        }
+
+       else { 
+          sum = sum + obj.getMeasure();
+           count++;
+       }
+     }
+     if (objects.length > 0) { return sum / count; }
+     else { return 0; }
+  }
+}
